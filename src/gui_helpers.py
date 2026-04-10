cf92fdb87de42f946798ef9dda58fc8447a875cb
datngo <ndat05808@gmail.com>
2026-04-10 16:38:57 +0700

    up

---
+++ src/gui_helpers.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+
+def set_text(text_area, content):
+    text_area.configure(state="normal")
+    text_area.delete("1.0", tk.END)
+    text_area.insert("1.0", content)
+    text_area.configure(state="disabled")
